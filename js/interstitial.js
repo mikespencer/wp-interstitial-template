c9a58516fcb304b4d47db9bad28c203b7b2d31c3
mikespencer <michael.spencer@washingtonpost.com>
2013-02-21 13:01:28 -0500

    jshint fix

--- js/interstitial.js
+++ js/interstitial.js
@@ -313,7 +313,7 @@
   Interstitial.prototype.getFlashVer = function(){
     var i,a,o,p,s="Shockwave",f="Flash",t=" 2.0",u=s+" "+f,v=s+f+".",rSW=new RegExp("^"+u+" (\\d+)");
     if((o=navigator.plugins)&&(p=o[u]||o[u+t])&&(a=p.description.match(rSW)))return a[1];
-    else if(!!(w.ActiveXObject))for(i=10;i>0;i--)try{if(!!(new ActiveXObject(v+v+i)))return i;}catch(e){}
+    else if(!!(w.ActiveXObject))for(i=10;i>0;i--)try{if(!!(new w.ActiveXObject(v+v+i)))return i;}catch(e){}
     return 0;
   };
 
